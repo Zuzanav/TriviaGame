3ae45484588d0f94563d3391f936d2d0203834ad
Zuzanav <zuzanavolny@gmail.com>
2019-07-25 20:25:58 -0700

    working timer, working questions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,112 +1,93 @@
-
 $(document).ready(function(){ 
 
-
 //========================================================================
-// VARIABLES 
-
-var correct = 0;
-var incorrect = 0;
-var question = "none";
-var input = "none";
-var answer = "none";
-
-// Start Game - when user presses Start Button
-$("#startButton").click(function() {
-
-
-    // alert the user how much time they have for trivia game
-    alert("You have 2 minutes to answer all the questions. Good Luck!");
-
-    // hide the start button
-    $('#startButton').hide();
-
-    // start the timer
-    setTimeout(twoMinutes, 1000 * 5);
-    
-    function twoMinutes() {
-        // 
-        $("#time-left").append("<h2> you have whatever seconds </h2>");
-        console.log("Time's Up!");
-
-    // show the questions
-    // $("#quiz-container").show();
-
-};
-
-});
-
-
-}) ;
-
-
-
-
-//=======================================================================
 
 // QUESTIONS & ANSWERS & CORRECT ANSWER
-/* var myQuestions = [
-	{
-		question: "What year was Shakespeare born?",
-		answers: {
-			a: '1616',
-			b: '1564',
-            c: '1732',
-            d: '1501'
-		},
-		correctAnswer: 'b'
-    },
-    {
-		question: "Who was Shakespeare's Wife?",
-		answers: {
-			a: 'Margaret Thatcher',
-			b: 'Anne Hathaway',
-            c: 'Halle Berry',
-            d: 'Elizabeth Townsend'
-		},
-		correctAnswer: 'b'
-    },
-    
-    {
-		question: "What was Shakespeare's first play?",
-		answers: {
-			a: 'Hamlet',
-			b: 'Macbeth',
-            c: 'Titus Andronicus',
-            d: 'Timon of Athens'
-		},
-		correctAnswer: 'c'
-    },
-    
-    {
-		question: "How many Greek plays has Shakespeare written?",
-		answers: {
-			a: '3',
-			b: '7',
-            c: '1',
-            d: '4'
-		},
-		correctAnswer: 'a'
-    },
-    
-    {
-		question: "What was William Shakespeare's father's profession?",
-		answers: {
-			a: 'priest',
-			b: 'book binder',
-            c: 'cobbler',
-            d: 'glove maker'
-		},
-		correctAnswer: 'd'
-    },
 
-]; */
+var myQuestions = [
+{
+question: "What year was Shakespeare born?",
+answers: ["1616", "1564", "1732", "1501"],
+correctAnswer: "1564"
+},
+{
+question: "Who was Shakespeare's Wife?",
+answers: ['Margaret Thatcher', 'Anne Hathaway','Halle Berry','Elizabeth Townsend'],
+   correctAnswer: 'Anne Hathaway'
+},
+
+{
+question: "What was Shakespeare's first play?",
+answers: ['Hamlet', 'Macbeth', 'Titus Andronicus','Timon of Athens'],
+correctAnswer: 'Titus Andronicus'
+},
+
+{
+question: "How many Greek plays has Shakespeare written?",
+answers: ['3', '7', '1', '4'],
+correctAnswer: '3'
+},
+
+{
+question: "What was William Shakespeare's father's profession?",
+answers: ['priest', 'book binder','cobbler', 'glove maker'],
+correctAnswer: 'glove maker'
+},
+]; 
 
 //========================================================================
 
-// Before Start of Game 
+// VARIABLES FOR TRIVIA GAME
 
-//document.getElementById('quiz-container').style.visibility = 'hidden';
+var game = {
+  correct: 0, 
+  incorrect: 0,
+  counter: 120, 
+  countdown: function() {
+    game.counter--;
+    $('#timer').html(game.counter);
+ 
+    // if the timer runs out, then run the function "game done" to trigger end of game
+      if(game.counter<= 0){
+        console.log("Time is up!");
+        //game.done();
+    }
+  }
+}
 
+// START OF GAME -----------------------------------------------------------------------------------
 
+  // - FUNCTION - WHEN START BUTTON IS CLICKED
+    $("#startButton").on('click', function() {
 
+      // alert the user how much time they have for trivia game
+        alert("You have 2 minutes to answer all the questions. Good Luck!");
+      
+        // TIMER ---------------------------------------------------------------------------------
+        // start the timer 
+        timer = setInterval(game.countdown, 1000);
+
+        // add text of time remaining to page
+        $("#smwrapper").prepend('<h2> Time Remaining: <span id="timer">120</span> seconds </h2>');
+
+        // HIDE START BUTTON ---------------------------------------------------------------------
+        $('#startButton').hide();
+
+        // QUESTIONS AND ANSWERS ------------------------------------------------------------------
+        // sort through my questions array
+        for(var i = 0; i < myQuestions.length; i++){
+
+        // append the questions to the div 
+        $("#smwrapper").append('<h4>' + myQuestions[i].question + '</h4>');
+
+        // create a loop to go through all the answers 
+        for(var j = 0; j < myQuestions[i].answers.length; j++){
+
+            // append the answers and give them a radio button to collect an input(or answer) from the user
+            $("#smwrapper").append("<input type='radio' name='question-"+i+"' value='"+myQuestions[i].answers[j]+"'>" +myQuestions[i].answers[j])
+            }
+        } //end for loop 
+
+      }); // END OF THE CLICK START BUTTON FUNCTION
+
+}) // END OF THE DOCUMENT READY FUNCTION
